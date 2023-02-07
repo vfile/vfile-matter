10cb93839694d1d3943a10be607d1b2c9ab7ffff
Titus Wormer <tituswormer@gmail.com>
2023-02-07 14:05:19 +0100

    Refactor code-style
    *   Add more docs to JSDoc
    *   Add support for `null` in input of API types
    *   Improve interface

--- index.js
+++ index.js
@@ -4,38 +4,53 @@
  * @typedef {import('yaml').DocumentOptions} DocumentOptions
  * @typedef {import('yaml').SchemaOptions} SchemaOptions
  * @typedef {import('yaml').ToJSOptions} ToJsOptions
- * @typedef {ParseOptions & DocumentOptions & SchemaOptions & ToJsOptions} YamlOptions
+ */
+
+/**
+ * @template Type
+ *   Type.
+ * @typedef {{[Key in keyof Type]: Type[Key]} & {}} Prettify
+ *   Flatten a TypeScript record.
+ */
+
+/**
+ * @typedef {Prettify<ParseOptions & DocumentOptions & SchemaOptions & ToJsOptions>} YamlOptions
+ *   Options for the YAML parser.
  *
  * @typedef Options
  *   Configuration (optional).
- * @property {boolean} [strip=false]
+ * @property {boolean | null | undefined} [strip=false]
  *   Remove the YAML front matter from the file.
- * @property {YamlOptions} [yaml]
+ * @property {YamlOptions | null | undefined} [yaml]
  *   Options for the YAML parser.
+ *
  *   These are passed as `x` in `yaml.parse('', x)`, which is equivalent to
- *   `ParseOptions & DocumentOptions & SchemaOptions & ToJsOptions`.
+ *   `ParseOptions`, `DocumentOptions`, `SchemaOptions`, and `ToJsOptions`.
  */
 
 import buffer from 'is-buffer'
 import yaml from 'yaml'
 
 /**
- * Parse the YAML front matter in a `vfile`, and add it as `file.data.matter`.
+ * Parse the YAML front matter in a `vfile` and expose it as `file.data.matter`.
  *
  * If no matter is found in the file, nothing happens, except that
  * `file.data.matter` is set to an empty object (`{}`).
  *
  * @template {VFile} File
+ *   File type.
  * @param {File} file
  *   Virtual file
- * @param {Options} [options]
+ * @param {Options | null | undefined} [options]
  *   Options
  * @returns {File}
  *   The given `file`
  */
-export function matter(file, options = {}) {
-  const strip = options.strip
-  const yamlOptions = options.yaml || {}
+// To do: next major: don’t return given file.
+export function matter(file, options) {
+  const options_ = options || {}
+  const strip = options_.strip
+  const yamlOptions = options_.yaml || {}
   let doc = String(file)
   const match =
     /^---(?:\r?\n|\r)(?:([\s\S]*?)(?:\r?\n|\r))?---(?:\r?\n|\r|$)/.exec(doc)
